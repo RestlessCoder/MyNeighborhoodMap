157acc34c03a89fe60e93802629ec8d5b76a0021
Chee Aik Lim <Chee Aik Lim>
2017-06-09 04:32:00 +0100

    Add Markers location on the map

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+	
+// Create a new blank for all the listing markers in global scope
+var markers = [];
+
 // Initialize the map
 function initMap() {
 	// Create a grey star marker
@@ -22,8 +26,64 @@ function initMap() {
         icon: greyStar,
         position: {lat: 54.596724, lng: -5.930082}
     });
-	
+
+}
+
+function searchVenuesQuery() {
+
+	var venueStr = $('#search-venues').val();
+
+	var fourSquareUrl = "https://api.foursquare.com/v2/venues/search?";
+	var fourSquareID = "client_id=4KRRWLQG1VJMY1GNF2CSWXNSPU5XRED3CFUACY4TOHZ53XOA&client_secret=LP11UFHMDQFYNYMYO42S4BD414ZJJQLMVK2OPNIEF0KIXEIJ&v=20170604";
+	var limitSearch = 15;
+	var fullUrl = fourSquareUrl + fourSquareID + "&near=belfast" + "&limit=" + limitSearch + "&query=" + venueStr;
+
+	// FourSquare AJAX request AJAX by using JSONP allows you to specify a callback function that is passed your JSON object. 
+	$.ajax({
+		url: fullUrl,
+		dataType : 'jsonp',
+		success: function(data) {
+
+			// Can be find after ajax successfully called the URL in the GoogleDevTol in Network section (FourSquare API)
+			var locationMarkers = data.response.venues;
+
+			clearOverlays();
+
+			for(var i = 0; i < locationMarkers.length; i++) {
+				// Get the lat position from the FourSquare API
+				var lat = locationMarkers[i].location.lat;
+				// Get the lng position from the FourSquare SPI
+				var lng = locationMarkers[i].location.lng;
+				// Get the place name from the FourSquare API
+				var name = locationMarkers[i].name;
+
+				var marker = new google.maps.Marker({
+					position: new google.maps.LatLng(lat, lng),
+					title: name,
+					map: map,
+					animation: google.maps.Animation.DROP
+				});
+
+				// Push the marker to our array of markers
+          		markers.push(marker);
+			}
+
+		}
+
+	});
+
+	return false;
+
+	// This will clear all the markers on the map
+	function clearOverlays() {
+	for (var i = 0; i < markers.length; i++ ) {
+	      markers[i].setMap(null);
+	    }
+	    // Resets the markers array
+	    markers = [];
+ 	} 
 }
 
 // Initialize the map
 initMap();
+
