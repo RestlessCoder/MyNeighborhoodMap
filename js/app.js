28eb8c96a12553f6c66e2153d27694946df8ab9e
RestlessCoder <nick.limca@hotmail.com>
2017-06-14 02:46:39 +0100

    Initial commit version 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,67 @@
-// Create a new blank for all the listing markers in global scope
-var markers = [];
+/* ========= Model ========= */ 
+
+// The Venue model that initialize and store venue information of the place 
+var VenueModel = function(data) {
+
+	this.name = ko.observable(data.venue.name);
+	this.formattedAddress = ko.observable(data.venue.location.formattedAddress);
+	this.url = ko.observable(data.venue.url);
+	this.rating = ko.observable(data.venue.rating);
+	this.categories = ko.observable(data.venue.categories[0].name);
+	this.price = ko.observable(data.venue.price.currency);
+
+}
+
+/* ========= ViewModel ========= */
+
+// This AppViewModel function expression is used inside to bind the HTML
+var AppViewModel = function() {
+	var self = this;
+	// Create a new blank for all the listing markers 
+	var markers = [];
+
+	// Initially blank input
+	self.exploreInputSearch = ko.observable(); 
+
+	// This will perform the search popular venue location function
+	self.searchVenueLocations = function() {
+
+		var fourSquareUrl = "https://api.foursquare.com/v2/venues/explore?";
+		var fourSquareID = "client_id=4KRRWLQG1VJMY1GNF2CSWXNSPU5XRED3CFUACY4TOHZ53XOA&client_secret=LP11UFHMDQFYNYMYO42S4BD414ZJJQLMVK2OPNIEF0KIXEIJ&v=20170604";
+		var limitSearch = "&limit=" + 15;
+		var location = "&near=belfast";
+		// This will query the venues from the input 
+		var query = "&query=" + self.exploreInputSearch();
+
+		var fullUrl = fourSquareUrl + fourSquareID + location + limitSearch + query;
+		//Retrieves JSON data from the FourSqaure API.
+		$.getJSON(fullUrl, function(data) {
+			// Can be find after ajax successfully called the URL in the GoogleDevTol in Network section (FourSquare API)
+			var fourSquareData = data.response.groups[0].items;
+ 			
+			for (var i = 0; i < fourSquareData.length; i++) {
+				// Get the lat position from the FourSquare API
+				var lat = fourSquareData[i].venue.location.lat;
+				// Get the lng position from the FourSquare SPI
+				var lng = fourSquareData[i].venue.location.lng;
+
+				var name = fourSquareData[i].venue.name;
+
+				console.log(name);
+				var marker = new google.maps.Marker({
+					position: new google.maps.LatLng(lat, lng),
+					title: name,
+					map: map,
+					animation: google.maps.Animation.DROP
+				});
+
+				// Push the marker to our array of markers
+          		markers.push(marker);
+
+			} // End for loop
+		});
+	}
+}	// End of AppViewModel
 
 // Initialize the map
 function initMap() {
@@ -26,86 +88,22 @@ function initMap() {
         position: {lat: 54.596724, lng: -5.930082}
     });
 
-    document.getElementById('submit-button').addEventListener("click", searchVenuesQuery);
+    $('#map-canvas').height($(window).height());
 
-}
-
-function searchVenuesQuery() {
-
-	var venueStr = $('#search-venues').val();
-
-	var fourSquareUrl = "https://api.foursquare.com/v2/venues/explore?";
-	var fourSquareID = "client_id=4KRRWLQG1VJMY1GNF2CSWXNSPU5XRED3CFUACY4TOHZ53XOA&client_secret=LP11UFHMDQFYNYMYO42S4BD414ZJJQLMVK2OPNIEF0KIXEIJ&v=20170604";
-	var limitSearch = 15;
-	var fullUrl = fourSquareUrl + fourSquareID + "&near=belfast" + "&limit=" + limitSearch + "&query=" + venueStr;
-
-	// FourSquare AJAX request AJAX by using JSONP allows you to specify a callback function that is passed your JSON object. 
-	$.ajax({
-		url: fullUrl,
-		dataType : 'jsonp',
-		success: function(data) {
-
-			// Can be find after ajax successfully called the URL in the GoogleDevTol in Network section (FourSquare API)
-			var fourSquareData = data.response.groups[0].items;
-
-			// This will clear all the markers on the map
-			clearOverlays();
-
-			// Create an infowindow variable
-    		var largeInfoWindow = new google.maps.InfoWindow();
-
-			for (var i = 0; i < fourSquareData.length; i++) {
-				// Get the lat position from the FourSquare API
-				var lat = fourSquareData[i].venue.location.lat;
-				// Get the lng position from the FourSquare SPI
-				var lng = fourSquareData[i].venue.location.lng;
-				// Get the place name from the FourSquare API
-				var name = fourSquareData[i].venue.name;
-
-				var marker = new google.maps.Marker({
-					position: new google.maps.LatLng(lat, lng),
-					title: name,
-					map: map,
-					animation: google.maps.Animation.DROP
-				});
-
-				// Push the marker to our array of markers
-          		markers.push(marker);
-          		  // Create an onclick event to open an infowindow at each marker.
-          		marker.addListener('click', function() {
-    				createInfoWindows(this, largeInfoWindow);
-    			});
-			} // End for loop
-		}
-
-	}); // End of Ajax
-
-	return false;
-
-	// This will clear all the markers on the map
-	function clearOverlays() {
-	for (var i = 0; i < markers.length; i++ ) {
-	      markers[i].setMap(null);
-	    }
-	    // Resets the markers array
-	    markers = [];
- 	} 
-}
-
- // This function is to create the infowindow when the marker is clicked. We'll only allow one infowindow which will open at the marker that is clicked
-function createInfoWindows(marker, infowindow) {
-	 // Check to make sure the infowindow is not already opened on this marker.
-	if (infowindow.marker != marker) {
-		infowindow.setContent('<div>' + marker.title + '</div>');
-		infowindow.marker = marker;
-		infowindow.open(map, marker);
-		 // Make sure the marker property is cleared if the infowindow is closed.
-		infowindow.addListener('closeclick', function() {
-			infowindow.marker = null;
-		});
+    //If the Google Maps API does not respond, it will load this error.
+    if (typeof google == 'undefined') {
+        $('#googleMap-Error').html('<h1>There are errors when retrieving map data. Please try again Later!</h1>'); 
+    		return;
 	}
 }
 
 // Initialize the map
 initMap();
+
+// This will apply the bindings for AppViewModel().
+$(document).ready(function() {
+
+    ko.applyBindings(new AppViewModel());
+
+});
 
